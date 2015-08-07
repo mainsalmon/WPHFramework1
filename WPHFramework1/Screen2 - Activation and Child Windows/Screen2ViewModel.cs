69c2d7123211e18dba7a0c5e9ab816c8a4da10fe
Will <whansen@uxceclipse.com>
2015-08-07 15:12:24 -0700

    adding files

---
+++ WPHFramework1/Screen2 - Activation and Child Windows/Screen2ViewModel.cs
@@ -0,0 +1,96 @@
+﻿using Caliburn.Micro;
+using MahApps.Metro.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MahApps.Metro.Controls.Dialogs;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Controls.Primitives;
+using System.Dynamic;
+
+namespace WPHFramework1
+{
+    class Screen2ViewModel : Conductor<Screen>    // string message is for demo; use specific object type for specific message types
+    {
+        private readonly IEventAggregator _eventAggregator;
+        private readonly IDialogCoordinator _dialogCoordinator;
+        private readonly IWindowManager _windowManager;
+
+        public Screen2ViewModel(IEventAggregator eventAggregator, IDialogCoordinator dialogCoordinator, IWindowManager windowManager)
+        {
+            _eventAggregator = eventAggregator;// .Subscribe(this) is happening in activate
+            _dialogCoordinator = dialogCoordinator;
+            _windowManager = windowManager;
+        }
+
+        #region Show Child Window
+
+         public void ShowWindow()
+         {
+             // shows non-modal child form (can now close the main form, but app stays up until child is closed)
+            var window = new Screen2ChildViewModel("Non Modal Child");
+           
+             _windowManager.ShowWindow(window);
+         
+             // It has no owner so centerowner must be changed to centerscreenWindowStartupLocation.CenterScreen
+         }
+
+        public void ShowModalDialogWindow(Screen2View view)
+        {
+            // shows child form modally
+            dynamic settings = new ExpandoObject();
+            settings.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+            settings.ResizeMode = ResizeMode.NoResize;
+            settings.Width = 450;
+           
+            Screen2ChildViewModel vm = new Screen2ChildViewModel("Modal Child");
+            _windowManager.ShowDialog(vm, null, settings);
+
+            // We don't really need to use this as a dialog, just as a modal child form.
+            // See Screen 1 for Mahapps dialogs which have all the build-in goodness.
+            
+            // Values of vm that have been set via the dialog are available here:
+            var x = vm.SomeValue;
+        
+        }
+
+        #endregion Show Child Window
+
+     
+
+        protected override void OnActivate()
+        {
+           // _eventAggregator.Subscribe(this);
+            _eventAggregator.PublishOnUIThread(new StatusMessage(Criticality.Informational, "Screen 2 Activated"));
+
+           // ShowTimedDialog("Screen Two Activated"); // can't show dialogs in activate because MetroWindow not up yet
+            base.OnActivate();
+        }
+
+        protected override void OnDeactivate(bool close)
+        {
+           // If the app is not closing, then show dialog
+            if ( Application.Current.MainWindow != null)
+            {
+                // ShowTimedDialog("Screen Two Deactivated"); // can't show dialogs in deactivate because MetroWindow is null too soon
+            }
+            _eventAggregator.PublishOnUIThread(new StatusMessage(Criticality.Informational, "Screen 2 Deactivated"));
+
+          //  _eventAggregator.Unsubscribe(this);
+            base.OnDeactivate(close);
+        }
+
+        public async void ShowTimedDialog(string message)
+        {
+            string msg = message + "; will close in 1 second.";
+            var dialog = new CustomDialog() { Title = msg };
+            await _dialogCoordinator.ShowMetroDialogAsync(this, dialog);
+            await Task.Delay(1000);
+            await _dialogCoordinator.HideMetroDialogAsync(this, dialog);
+        }
+    }
+}
---
+++ WPHFramework1/Shell - Main Menu/ShellViewModel.cs
@@ -0,0 +1,158 @@
+namespace WPHFramework1 {
+
+    using Caliburn.Micro;
+    using MahApps.Metro.Controls.Dialogs;
+    using System.Collections.ObjectModel;
+    using System.Threading.Tasks;
+
+    public class ShellViewModel : Conductor<object>, IShell, IHandle<StatusMessage>
+    {
+        IWindowManager _windowManager;
+        IEventAggregator _eventAggregator;
+
+        public ShellViewModel(IEventAggregator ea,  IWindowManager wm)
+        {
+            _windowManager = wm;
+            _eventAggregator = ea;
+            DisplayName = "WPH Framework1 (Mahapps + Caliburn)";
+            StatusMessageText = string.Empty;
+        }
+
+        #region Lifecycle stuff
+
+        protected override void OnInitialize(){
+            base.OnInitialize();
+            _eventAggregator.Subscribe(this);
+            _settingVM = new SettingsViewModel(_eventAggregator);
+            ShowScreen1(); // Initial default
+        }
+
+        protected override void OnDeactivate(bool close)
+        {
+            _eventAggregator.Unsubscribe(this);
+            base.OnDeactivate(close);
+        }
+
+        #endregion Lifecycle Stuff
+
+        #region Settings Flyout
+
+        private SettingsViewModel _settingVM;
+        public SettingsViewModel SettingsVM
+        {
+            get { return _settingVM; }
+        }
+
+        public void ShowSettingsFlyout(ShellView view)
+        {
+            var flyout = view.settingsFlyout;
+            flyout.IsOpen = !flyout.IsOpen;
+        }
+
+        #endregion Settings Flyout
+
+        #region Show Screens
+
+        public void ShowScreen1()
+        {
+            ActivateItem(new Screen1ViewModel(DialogCoordinator.Instance));
+            RefreshMenuButtonGuards();
+        }
+
+        public void ShowScreen2()
+        {
+            ActivateItem(new Screen2ViewModel(_eventAggregator, DialogCoordinator.Instance, _windowManager));
+            RefreshMenuButtonGuards();
+        }
+
+        public void ShowScreen3()
+        {
+            ActivateItem(new Screen3ViewModel());
+            RefreshMenuButtonGuards();
+        }
+
+        public void ShowScreen4()
+        {
+            ActivateItem(new Screen4ViewModel());
+            RefreshMenuButtonGuards();
+        }
+
+        public void ShowScreen5()
+        {
+            ActivateItem(new Screen5ViewModel());
+            RefreshMenuButtonGuards();
+        }
+
+        #endregion Show Screens
+
+        #region Disable Menu Button if it's screen is current selection
+
+        private void RefreshMenuButtonGuards()
+        {
+            NotifyOfPropertyChange(() => CanShowScreen1);
+            NotifyOfPropertyChange(() => CanShowScreen2);
+            NotifyOfPropertyChange(() => CanShowScreen3);
+            NotifyOfPropertyChange(() => CanShowScreen4);
+            NotifyOfPropertyChange(() => CanShowScreen5);
+        }
+
+        public bool CanShowScreen1
+        {
+            get { return ActiveItem == null || ActiveItem.GetType() != typeof(Screen1ViewModel); }
+        }
+
+        public bool CanShowScreen2
+        {
+            get { return ActiveItem == null || ActiveItem.GetType() != typeof(Screen2ViewModel); }
+        }
+
+        public bool CanShowScreen3
+        {
+            get { return ActiveItem == null || ActiveItem.GetType() != typeof(Screen3ViewModel); }
+        }
+
+        public bool CanShowScreen4
+        {
+            get { return ActiveItem == null || ActiveItem.GetType() != typeof(Screen4ViewModel); }
+        }
+
+        public bool CanShowScreen5
+        {
+            get { return ActiveItem == null || ActiveItem.GetType() != typeof(Screen5ViewModel); }
+        }
+
+        #endregion Disable Menu Button if it's screen is current selection
+
+        #region Handle Status Message
+
+        public async void Handle(StatusMessage messageObj)
+        {
+            // Handle the message here.
+            // Messages can be differentiated by type.  So create various message objects
+            // and handle each separately.  also note polymorphism.  So handler for object will also handle string msg
+            // and if both an object and string handlers are defined, both will be called.
+        
+            StatusMessageText = messageObj.Message;
+
+            await Task.Delay(3000);
+            StatusMessageText = string.Empty;
+        }
+
+        private string _statusMessageText;
+        public string StatusMessageText
+        {
+            get { return _statusMessageText; }
+            set {
+                _statusMessageText = value;
+                NotifyOfPropertyChange(() => StatusMessageText);
+                NotifyOfPropertyChange(() => IsStatusMessageSet);
+            }
+        }
+
+        public bool IsStatusMessageSet
+        {
+            get { return StatusMessageText != string.Empty; }
+        }
+        #endregion Handle Status Message 
+    }
+}
