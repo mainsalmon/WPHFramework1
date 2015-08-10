5d7526da9b7c25540c75e18452d315aa4aa2652c
Will <whansen@uxceclipse.com>
2015-08-10 09:34:57 -0700

    Added charting

--- WPHFramework1/Screen4 - Charting/Screen4ViewModel.cs
+++ WPHFramework1/Screen4 - Charting/Screen4ViewModel.cs
@@ -1,13 +1,30 @@
 ﻿using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WPHFramework1
 {
-    class Screen4ViewModel : Screen
+    public class Screen4ViewModel : Screen
     {
+
+        public Screen4ViewModel()
+        {
+            ChartData = new ObservableCollection<KeyValuePair<string, int>>();
+            ChartData.Add(new KeyValuePair<string, int>("Developer", 7));
+            ChartData.Add(new KeyValuePair<string, int>("Misc", 5));
+            ChartData.Add(new KeyValuePair<string, int>("Tester", 3));
+        }
+
+        private ObservableCollection<KeyValuePair<string, int>> _chartData;
+
+        public ObservableCollection<KeyValuePair<string, int>> ChartData
+        {
+            get { return _chartData; }
+            set { _chartData = value; }
+        }
     }
 }
