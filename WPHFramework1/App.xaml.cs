03d4cd56108d335d5868ac04a35d6d98a7a36e3e
Will <whansen@uxceclipse.com>
2015-08-27 15:03:02 -0700

    Added notes on changing themes

--- WPHFramework1/App.xaml.cs
+++ WPHFramework1/App.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows;
 
 namespace WPHFramework1
 {
+    using MahApps.Metro;
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
@@ -19,6 +20,23 @@ namespace WPHFramework1
         {
             InitializeComponent();
         }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // To set the accent and theme...
+            // 1) get the theme from the current application
+            //var theme = ThemeManager.DetectAppStyle(Application.Current);
+
+            // 2) now set the Green accent and dark theme
+            //ThemeManager.ChangeAppStyle(Application.Current,
+            //                            ThemeManager.GetAccent("Green"),
+            //                            ThemeManager.GetAppTheme("BaseDark"));
+
+            // for available 'stock' accents and themes and more info, see http://mahapps.com/guides/styles.html#app
+
+            base.OnStartup(e);
+        }
+
     }
 
 }
